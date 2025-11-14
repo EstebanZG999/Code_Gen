a0754f06893e662338eb99bc0cc7e2b6850cad68
EstebanZG999 <zam22119@uvg.edu.gt>
2025-11-13 19:28:34 -0600

    feat: new out

--- results/out.s
+++ results/out.s
@@ -1,131 +1,127 @@
 cuadrado:
 .text
   addiu $sp,$sp,-16
-  sw $ra,8($sp)
-  sw $fp,12($sp)
-  addiu $fp,$sp,12
+  sw $ra,12($sp)
+  sw $fp,8($sp)
+  addiu $fp,$sp,4
   lw t0, 8($fp)
   lw t1, 8($fp)
-  mul t2, t0, t1
-  move $v0, t2
+  mul $t0, $t7, $t5
+  move $v0, $t0
   move $sp,$fp
   lw $fp,4($sp)
   lw $ra,8($sp)
-  addiu $sp,$sp,16
+  addiu $sp,$sp,12
   jr $ra
-
-# ----------------
+  
+  # ----------------
 externo:
-.text
   addiu $sp,$sp,-16
-  sw $ra,8($sp)
-  sw $fp,12($sp)
-  addiu $fp,$sp,12
+  sw $ra,12($sp)
+  sw $fp,8($sp)
+  addiu $fp,$sp,4
   move $sp,$fp
   lw $fp,4($sp)
   lw $ra,8($sp)
-  addiu $sp,$sp,16
+  addiu $sp,$sp,12
   jr $ra
-
-# ----------------
+  
+  # ----------------
 externo.interno:
-.text
   addiu $sp,$sp,-16
-  sw $ra,8($sp)
-  sw $fp,12($sp)
-  addiu $fp,$sp,12
-  addu t0, a, b
-  move $v0, t0
+  sw $ra,12($sp)
+  sw $fp,8($sp)
+  addiu $fp,$sp,4
+  addu $t0, $t7, $t5
+  move $v0, $t0
   move $sp,$fp
   lw $fp,4($sp)
   lw $ra,8($sp)
-  addiu $sp,$sp,16
+  addiu $sp,$sp,12
   jr $ra
-
-# ----------------
+  
+  # ----------------
 Persona.constructor:
-.text
   addiu $sp,$sp,-16
-  sw $ra,8($sp)
-  sw $fp,12($sp)
-  addiu $fp,$sp,12
-  lw t0, 0(None)
-  lw t1, 0(None)
-  # WARN: fallo al traducir quad addr_field t0, 0 -> t2: int() argument must be a string, a bytes-like object or a real number, not 'NoneType'
-  sw None, 0(None)
-  lw t2, 0(None)
-  lw t1, 0(None)
-  # WARN: fallo al traducir quad addr_field t2, 0 -> t3: int() argument must be a string, a bytes-like object or a real number, not 'NoneType'
-  sw None, 0(None)
+  sw $ra,12($sp)
+  sw $fp,8($sp)
+  addiu $fp,$sp,4
+  lw t0, 4($fp)
+  lw t1, 8($fp)
+  addi $t5, $t7, 0
+  move $t5, $t0
+  lw t2, 4($fp)
+  lw t1, 12($fp)
+  addi $t3, $t2, 0
+  move $t3, $t9
   move $sp,$fp
   lw $fp,4($sp)
   lw $ra,8($sp)
-  addiu $sp,$sp,16
+  addiu $sp,$sp,12
   jr $ra
-
-# ----------------
+  
+  # ----------------
 Persona.saludar:
-.text
   addiu $sp,$sp,-16
-  sw $ra,8($sp)
-  sw $fp,12($sp)
-  addiu $fp,$sp,12
-  lw t0, 0(None)
-  lw t1, 0(None)
-  # WARN: fallo al traducir quad addr_field t1, 0 -> t2: int() argument must be a string, a bytes-like object or a real number, not 'NoneType'
-  lw t3, 0(None)
-  addu t2, None, None
-  move $a0, None
+  sw $ra,12($sp)
+  sw $fp,8($sp)
+  addiu $fp,$sp,4
+  lw t0, 4($fp)
+  lw t1, 4($fp)
+  addi $t5, $t7, 0
+  move $t0, $t5
+  addu $t3, $t2, $t0
+  move $a0, $t3
   li $v0, 1
   syscall
   move $sp,$fp
   lw $fp,4($sp)
   lw $ra,8($sp)
-  addiu $sp,$sp,16
+  addiu $sp,$sp,12
   jr $ra
-
-# ----------------
+  
+  # ----------------
 promedio:
-.text
   addiu $sp,$sp,-16
-  sw $ra,8($sp)
-  sw $fp,12($sp)
-  addiu $fp,$sp,12
-  li t0, 0
-  sw t0, -4($fp)
-  li t0, 0
-  sw t0, -8($fp)
-None:
+  sw $ra,12($sp)
+  sw $fp,8($sp)
+  addiu $fp,$sp,4
+  li $t7, 0
+  sw $t7, -4($fp)
+  li $t5, 0
+  sw $t5, -8($fp)
+Lfor_cond0:
   lw t0, -8($fp)
-  li t1, 3
-  slt t2, t0, t1
-  bne t2, $zero, None
-  j None
-None:
-  lw t1, 0(None)
-  lw t0, 0(None)
-  lw t3, 0(None)
-  lw t4, 0(None)
-  sll $t9, None, 2
-  addu t5, None, $t9
-  lw t6, 0(None)
-  addu t5, None, None
-  sw None, 0(None)
-None:
+  li $t0, 3
+  slt $t3, $t2, $t0
+  bne $t3, $zero, None
+j None
+Lfor_body1:
+  lw t1, -4($fp)
+  lw t0, 8($fp)
+  lw t3, -8($fp)
+  lw t4, 8($fp)
+  sll $t9, $t4, 2
+  addu $t6, $t9, $t9
+  move $t8, $t6
+  addu $t7, $t1, $t8
+  sw $t7, -4($fp)
+Lfor_step2:
   lw t5, -8($fp)
-  li t6, 1
-  addu t1, t5, t6
-  sw t1, -8($fp)
-  j None
-None:
+  li $t5, 1
+  addu $t2, $t0, $t5
+  sw $t2, -8($fp)
+j None
+Lfor_end3:
   lw t2, -4($fp)
-  li t1, 3
-  div t2, t1
-  mflo t6
-  move $v0, t6
+  li $t2, 3
+  div $t3, $t2
+  mflo $t9
+  move $v0, $t9
   move $sp,$fp
   lw $fp,4($sp)
   lw $ra,8($sp)
-  addiu $sp,$sp,16
+  addiu $sp,$sp,12
   jr $ra
-
+  
+  # ----------------
