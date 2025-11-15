abcd2726566e71423de7b678f1e34fda92aef6bb
EstebanZG999 <zam22119@uvg.edu.gt>
2025-11-14 22:44:02 -0600

    new out

--- results/out.s
+++ results/out.s
@@ -6,10 +6,10 @@ cuadrado:
   sw $fp,8($sp)
   addiu $fp,$sp,4
   # --- fin prologo de cuadrado ---
-  lw t0, 12($fp)
-  lw t1, 12($fp)
-  mul $t7, $t3, $t2
-  move $v0, $t7
+  lw $t8, 12($fp)
+  lw $t3, 12($fp)
+  mul $t5, $t8, $t3
+  move $v0, $t5
   # --- epilogo de cuadrado ---
   move $sp,$fp
   lw $fp,4($sp)
@@ -44,8 +44,8 @@ externo.interno:
   sw $fp,8($sp)
   addiu $fp,$sp,4
   # --- fin prologo de externo.interno ---
-  addu $t7, $t3, $t2
-  move $v0, $t7
+  addu $t5, $t8, $t3
+  move $v0, $t5
   # --- epilogo de externo.interno ---
   move $sp,$fp
   lw $fp,4($sp)
@@ -63,14 +63,14 @@ Persona.constructor:
   sw $fp,8($sp)
   addiu $fp,$sp,4
   # --- fin prologo de Persona.constructor ---
-  lw t0, 8($fp)
-  lw t1, 12($fp)
-  addi $t2, $t3, 0
-  move $t2, $t7
-  lw t2, 8($fp)
-  lw t1, 16($fp)
-  addi $t5, $t9, 0
-  move $t5, $t1
+  lw $t8, 12($fp)
+  lw $t3, 16($fp)
+  addi $t5, $t8, 0
+  sw $t3, 0($t5)
+  lw $t5, 12($fp)
+  lw $t6, 20($fp)
+  addi $t7, $t5, 0
+  sw $t6, 0($t7)
   # --- epilogo de Persona.constructor ---
   move $sp,$fp
   lw $fp,4($sp)
@@ -88,12 +88,12 @@ Persona.saludar:
   sw $fp,8($sp)
   addiu $fp,$sp,4
   # --- fin prologo de Persona.saludar ---
-  lw t0, 8($fp)
-  lw t1, 8($fp)
-  addi $t2, $t3, 0
-  move $t7, $t2
-  addu $t5, $t9, $t7
-  move $a0, $t5
+  lw $t8, 12($fp)
+  lw $t3, 12($fp)
+  addi $t5, $t3, 0
+  lw $t6, 0($t5)
+  addu $t0, $t7, $t6
+  move $a0, $t0
   li $v0, 1
   syscall
   # --- epilogo de Persona.saludar ---
@@ -113,38 +113,38 @@ promedio:
   sw $fp,8($sp)
   addiu $fp,$sp,4
   # --- fin prologo de promedio ---
+  li $t8, 0
+  sw $t8, -4($fp)
   li $t3, 0
-  sw $t3, -4($fp)
-  li $t2, 0
-  sw $t2, -8($fp)
+  sw $t3, -8($fp)
 Lfor_cond0:
-  lw t0, -8($fp)
-  li $t7, 3
-  slt $t5, $t9, $t7
-  bne $t5, $zero, None
-j None
+  lw $t5, -8($fp)
+  li $t6, 3
+  slt $t7, $t5, $t6
+  bne $t7, $zero, Lfor_body1
+j Lfor_end3
 Lfor_body1:
-  lw t1, -4($fp)
-  lw t0, 12($fp)
-  lw t3, -8($fp)
-  lw t4, 12($fp)
-  sll $t9, $t0, 2
-  addu $t6, $t1, $t9
-  move $t4, $t6
-  addu $t3, $t8, $t4
-  sw $t3, -4($fp)
+  lw $t7, -4($fp)
+  lw $t0, 12($fp)
+  lw $t4, -8($fp)
+  lw $t2, 12($fp)
+  sll $t1, $t4, 2
+  addu $t1, $t2, $t1
+  lw $t9, 0($t1)
+  addu $t8, $t7, $t9
+  sw $t8, -4($fp)
 Lfor_step2:
-  lw t5, -8($fp)
-  li $t2, 1
-  addu $t9, $t7, $t2
-  sw $t9, -8($fp)
-j None
+  lw $t9, -8($fp)
+  li $t3, 1
+  addu $t5, $t9, $t3
+  sw $t5, -8($fp)
+j Lfor_cond0
 Lfor_end3:
-  lw t2, -4($fp)
-  li $t5, 3
-  div $t1, $t5
-  mflo $t0
-  move $v0, $t0
+  lw $t5, -4($fp)
+  li $t6, 3
+  div $t5, $t6
+  mflo $t7
+  move $v0, $t7
   # --- epilogo de promedio ---
   move $sp,$fp
   lw $fp,4($sp)
